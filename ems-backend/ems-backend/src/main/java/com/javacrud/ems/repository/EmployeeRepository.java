8e01221b6efbd6b106e4737d4775033baa8bc0f1
Sowndaryakp <sonalshetty.1999@gmail.com>
2024-07-28 19:59:13 +0530

    ems backend completed

--- ems-backend/ems-backend/src/main/java/com/javacrud/ems/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.javacrud.ems.repository;
-
-import com.javacrud.ems.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
-
